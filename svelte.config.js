f58d9c3ecb37c3aaa95d0a5aebe43fc7a091524b
Johan <johan@wtmg.se>
2023-04-25 13:58:04 +0200

    fix config for cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -9,17 +9,14 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			// See below for an explanation of these options
-			routes: {
-				 include: ['/*'],
-				 exclude: ['<all>']
-			}
-	  })
-	}
+			 // See below for an explanation of these options
+			 routes: {
+				  include: ['/*'],
+				  exclude: ['<all>']
+			 }
+		})
+  }
 };
 
 export default config;
